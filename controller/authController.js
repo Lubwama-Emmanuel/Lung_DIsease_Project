8a9c3a6c6117e4da8f57bfc8d3a9a4a98378aa45
Rex <lubwamaemmanuel1@gmail.com>
2022-07-25 12:55:36 -0700

    Repairs

--- controller/authController.js
+++ controller/authController.js
@@ -6,37 +6,41 @@ const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-
-exports.signUp = catchAsync(async (req, res, next) => {
-  const user = await User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-  });
+const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  res.status(201).json({
+  res.status(statusCode).json({
     status: "success",
     token,
     data: {
       user,
     },
   });
+};
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.signIn = catchAsync(async (req, res, next) => {
-  const { email, password } = await User.find(req.body);
-  
+  const { email, password } = req.body;
+  // 1) Check if user provided email and password
   if (!email || !password) {
     return next(new AppError("Please provide your Email and Password", 400));
   }
   // check if user exists
   const user = await User.findOne({ email }).select("+password");
+  const correct = await user.correctPassword(password, user.password);
 
-  if (!user || !(await correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect email or password", 401));
+  if (!user || !correct) {
+    return next(new AppError("Incorrect email or password", 400));
   }
+
   const token = signToken(user._id);
   res.status(201).json({
     status: "Success",
